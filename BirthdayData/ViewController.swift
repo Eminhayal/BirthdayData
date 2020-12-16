93a9f129adc37b53fa9ef227e9c1dca82191a435
Eminhayal <eminhayal768@gmail.com>
2020-12-16 08:51:49 +0300

    Initial Commit

---
+++ BirthdayData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BirthdayData
+//
+//  Created by Emin Hayal on 16.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
