e97624e671c38bb29eab33993124391d8bc1ef7f
Eminhayal <eminhayal768@gmail.com>
2020-12-19 22:16:30 +0300

    iOS öğrenirken basit program. Küçük verileri tutmak için kullanılan userDefault.Standard.Set komutunun kısa bir örneği

--- BirthdayData/ViewController.swift
+++ BirthdayData/ViewController.swift
@@ -8,12 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var birthdayTextField: UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var birthdayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let storedName = UserDefaults.standard.object(forKey:"name")
+        let storedBirthday = UserDefaults.standard.object(forKey: "birthday")
+        
+        if let newName = storedName as? String {
+            nameLabel.text = "Name : \(newName)"
+        }
+        
+        if let newBirthday = storedBirthday as? String {
+            birthdayLabel.text = "Birthday : \(newBirthday)"
+        }
     }
 
-
+    @IBAction func saveButton(_ sender: Any) {
+        
+        UserDefaults.standard.set(nameTextField.text, forKey: "name")
+        UserDefaults.standard.set(birthdayTextField.text, forKey: "birthday")
+        
+        nameLabel.text = "Name : \(nameTextField.text!)"
+        birthdayLabel.text = "Birthday : \(birthdayTextField.text!)"
+    }
+    @IBAction func deleteButton(_ sender: Any) {
+        
+        let storedName = UserDefaults.standard.object(forKey: "name")
+        let storedBirthday = UserDefaults.standard.object(forKey: "birthday")
+        
+        if ( storedName as? String ) != nil {
+            UserDefaults.standard.removeObject(forKey: "name")
+            nameLabel.text = "Name : "
+        }
+        
+        if(storedBirthday as? String) != nil {
+            UserDefaults.standard.removeObject(forKey: "birthday")
+            birthdayLabel.text = "Birthday : "
+        }
+        
+        
+    }
+    
 }
 
